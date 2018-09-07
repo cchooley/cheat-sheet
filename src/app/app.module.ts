572319d3ac5dbbb5cf32eec9ac1729cd93dcb4ec
Conor Hooley <cchooley@gmail.com>
2018-09-07 02:48:24 -0600

    Initial commit. MDB integrated, background image buttons and short text added to landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { LandComponent } from './components/land/land.component';
+import { RegisterButtonComponent } from './components/register-button/register-button.component';
+import { LoginButtonComponent } from './components/login-button/login-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandComponent,
+    RegisterButtonComponent,
+    LoginButtonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
